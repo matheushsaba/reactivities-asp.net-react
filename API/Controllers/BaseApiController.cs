665dae7014ee958a79b37176cf802aad971b9c22
Matheus Sabadin <matheushs15@hotmail.com>
2023-08-03 22:01:15 -0300

    Add controllers

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BaseApiController : Controller
+    {
+        [ApiController]
+        [Route("api/[controller]")]
+        public class BaseAPiController : ControllerBase
+        {
+
+        }
+    }
+}
