f80ace6a4d53108b56567b0fe51fbfcbab997521
Matheus Sabadin <matheushs15@hotmail.com>
2023-08-03 21:36:18 -0300

    Create mock database data

--- API/Program.cs
+++ API/Program.cs
@@ -35,7 +35,8 @@ IServiceProvider services = scope.ServiceProvider;
 try
 {
     DataContext context = services.GetRequiredService<DataContext>();
-    context.Database.Migrate();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
 }
 catch (Exception ex)
 {
